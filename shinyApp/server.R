e5d2283104d4eff74db23fd1aa8bedd6e5a55ee9
Richard Hardy <rphardy497@gmail.com>
2020-01-28 01:11:18 +1100

    bare-bones first shiny app draft

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,26 +1,24 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw facet plot of lifeExp vs gdpPercap
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$gdplife <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        # generate bins based on input$year from ui.R
+        x    <- gapminder$gdpPercap
+        year <- seq(min(x), max(x), length.out = input$year + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+        gap_bit <- subset(gapminder, year == input$year & continent != "Oceania")
+        gap_bit <- gap_bit[with(gap_bit, order(continent, year, -1 * pop)), ]
+        ggplot(gap_bit, aes(x = gdpPercap, y = lifeExp, size = pop)) +
+            scale_x_log10(limits = c(150, 115000)) + ylim(c(16, 96)) +
+            geom_point(pch = 21, color = 'grey20', show.legend = FALSE) +
+            scale_size_area(max_size = 40) +
+            facet_wrap(~ continent) + coord_fixed(ratio = 1/43) +
+            aes(fill = country) + scale_fill_manual(values = country_colors) +
+            theme_bw() + theme(strip.text = element_text(size = rel(1.1)))
     })
 
 })
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,33 +1,25 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a series of plots
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Gapminder - Life Expectancy by GDP over time"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput(inputId = "year",
+                        label = "slide by year",
+                        min = 1952,
+                        max = 2007,
+                        step = 5,
+                        value = 1987)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("gdplife")
         )
     )
 ))
